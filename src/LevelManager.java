17cec393171b66ecc9d03e840a83e42f851b6e2b
sehyeonn <a3920679@naver.com>
2024-11-26 15:51:40 +0900

    레벨 아이템 구성 변경

--- src/LevelManager.java
+++ src/LevelManager.java
@@ -30,17 +30,17 @@ public class LevelManager {
 		itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
 		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
 		itemTemplates.add(new Item("콜라 빈 페트병", "플라스틱", "images/petbottle_none_cola.png"));
+		
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
 		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
 
-		itemTemplates.add(new Item("종이", "종이", "images/paper.png"));
-		itemTemplates.add(new Item("포스트잇", "종이", "images/paper_2.png"));
-		itemTemplates.add(new Item("신문지", "종이", "images/paper_3.png"));
+		itemTemplates.add(new Item("참치캔", "캔류", "images/can_dong.png"));
+		itemTemplates.add(new Item("빈 스팸 캔", "캔류", "images/can_spam.png"));
 
 		List<Bin> bins = new ArrayList<>();
 		bins.add(new Bin("플라스틱", "images/plastic_bin.png"));
 		bins.add(new Bin("유리", "images/glass_bin.png"));
-		bins.add(new Bin("종이", "images/paper_bin.png")); //캔으로 수정하기
+		bins.add(new Bin("캔류", "images/can_bin.png")); //캔으로 수정하기
 
 		return new LevelData(1, itemTemplates, bins);
 	}
@@ -52,14 +52,13 @@ public class LevelManager {
 		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_bowl.png"));
 		itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
 		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
-		itemTemplates.add(new Item("빈 페트병", "플라스틱", "images/petbottle_black.png"));
-		itemTemplates.add(new Item("콜라 빈 페트병", "플라스틱", "images/petbottle_none_cola.png"));
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
 		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
 		itemTemplates.add(new Item("종이", "종이", "images/paper.png"));
 		itemTemplates.add(new Item("포스트잇", "종이", "images/paper_2.png"));
 		itemTemplates.add(new Item("신문지", "종이", "images/paper_3.png"));
 		itemTemplates.add(new Item("휴지", "일반", "images/tissue.png"));
+		itemTemplates.add(new Item("프링글스 통", "일반", "images/pringles.png"));
 
 		List<Bin> bins = new ArrayList<>();
 		bins.add(new Bin("플라스틱", "images/plastic_bin.png"));
@@ -75,17 +74,15 @@ public class LevelManager {
 		List<Item> itemTemplates = new ArrayList<>();
 
 		itemTemplates.add(new Item("빈 페트병", "페트", "images/petbottle_black.png"));
-		//itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
-		//itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
-		itemTemplates.add(new ComplexItem("라벨이 있는 페트병", "페트#cutter#비닐", "images/petbottle_cola_label.png", new Item("페트병", "페트", "images/petbottle_none_cola.png"), new Item("라벨", "비닐", "images/lable_vinyl.png"), "cutter"));
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
-		itemTemplates.add(new ComplexItem("음료수가 있는 페트병", "페트#sink#", "images/petbottle_sink.png", new Item("페트병", "페트", "images/petbottle_sink_none.png"), "sink"));
 		itemTemplates.add(new Item("지퍼팩", "비닐", "images/vinyl.png"));
 		itemTemplates.add(new Item("비닐봉지", "비닐", "images/vinyl_2.png"));
 		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
+		itemTemplates.add(new ComplexItem("라벨이 있는 페트병", "라벨이 있는 페트", "images/petbottle_cola_label.png", new Item("페트병", "페트", "images/petbottle_none_cola.png"), new Item("라벨", "비닐", "images/lable_vinyl.png"), "커터"));
+		itemTemplates.add(new ComplexItem("음료수가 있는 유리병", "음료수가 있는 유리", "images/glassbottle_sink.png", new Item("빈 유리병", "유리", "images/glassbottle_sink_none.png"), "싱크대"));
+		itemTemplates.add(new ComplexItem("음료수가 있는 페트병", "음료수가 있는 페트", "images/petbottle_sink.png", new Item("페트병", "페트", "images/petbottle_sink_none.png"), "싱크대"));
 		
 		List<Bin> bins = new ArrayList<>();
-		//bins.add(new Bin("플라스틱", "images/plastic_bin.png"));
 		bins.add(new Bin("페트", "images/pet_bin.png"));
 		bins.add(new Bin("유리", "images/glass_bin.png"));
 		bins.add(new Bin("비닐", "images/vinyl_bin.png"));
